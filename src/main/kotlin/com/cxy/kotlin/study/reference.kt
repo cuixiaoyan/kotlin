0347121145e85a7c0e936c9d4adace94e16cb0a9
cuixiaoyan <56696508@qq.com>
2019-11-05 16:07:34 +0800

    spring kotlin for循环测试

--- src/main/kotlin/com/cxy/kotlin/study/reference.kt
+++ src/main/kotlin/com/cxy/kotlin/study/reference.kt
@@ -1,5 +1,7 @@
 package com.cxy.kotlin.study
 
+import javafx.scene.shape.Rectangle
+
 /**
  * 官网学习
  */
@@ -39,12 +41,150 @@ fun main(args: Array<String>) {
     //条件表达式
     //println(maxOf(1, 2))
 
-    //同时提交到码云  重新测试
+    //当某个变量的值可以为 null 的时候，必须在声明处的类型后添加 ? 来标识该引用可为空
+    //println(parseInt("1"))
+    //使用返回可空值的函数:
+    //println(printProduct("1","java"))
+
+    //类型检测与自动类型转换
+    //is 运算符检测一个表达式是否某类型的一个实例。
+    // 如果一个不可变的局部变量或属性已经判断出为某类型，那么检测后的分支中可以直接当作该类型使用，无需显式转换
+    //println(getStringLength(999))
+
+    //for 循环
+//    val items = listOf("apple", "banana", "kiwifruit")
+//    for (item in items) {
+//        println(item)
+//    }
+    //或者
+//    val items = listOf("apple", "banana", "kiwifruit")
+//    for (index in items.indices) {
+//        println("item at $index is ${items[index]}")
+//    }
+
+
+    //when 表达式
+    //println(describe(9))
+
+    //使用区间（range）
+    //使用 in 运算符来检测某个数字是否在指定区间内:
+//    val x = 10
+//    val y = 9
+//    if (x in 1..y+1) {
+//        println("fits in range")
+//    }
+
+    //检测某个数字是否在指定区间外
+//    val list = listOf("a", "b", "c")
+//
+//    if (-1 !in 0..list.lastIndex) {
+//        println("-1 is out of range")
+//    }
+//    if (list.size !in list.indices) {
+//        println("list size is out of valid list indices range, too")
+//    }
+
+    //区间迭代
+//    for (x in 1..5) {
+//        print(x)
+//    }
+
+    //或数列迭代
+//    for (x in 1..10 step 2) {
+//        print(x)
+//    }
+//    println()
+//    for (x in 9 downTo 0 step 3) {
+//        print(x)
+//    }
+
+
+    //Collections
+    //对集合进行迭代
+//    val items = listOf("apple", "banana", "kiwifruit")
+//    for (item in items) {
+//        println(item)
+//    }
+//    //使用 in 运算符来判断集合内是否包含某实例
+//    when {
+//        "orange" in items -> println("juicy")
+//        "apple" in items -> println("apple is fine too")
+//    }
+//    //使用 lambda 表达式来过滤（filter）与映射（map）集合
+//    val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+//    fruits
+//            .filter { it.startsWith("a") }
+//            .sortedBy { it }
+//            .map { it.toUpperCase() }
+//            .forEach { println(it) }
+
+    //创建基本类及其实例
+    //val rectangle = Rectangle(5.0, 2.0)
+
+
 
 
 
 
 }
+
+
+
+//fun describe(obj: Any): String =
+//        when (obj) {
+//            1          -> "One"
+//            "Hello"    -> "Greeting"
+//            is Long    -> "Long"
+//            !is String -> "Not a string"
+//            else       -> "Unknown"
+//        }
+
+
+//fun getStringLength(obj: Any): Int? {
+//    // `obj` 在 `&&` 右边自动转换成 `String` 类型
+//    if (obj is String && obj.length > 0) {
+//        return obj.length
+//    }
+//
+//    return null
+//}
+
+//fun getStringLength(obj: Any): Int? {
+//    if (obj !is String) return null
+//
+//    // `obj` 在这一分支自动转换为 `String`
+//    return obj.length
+//}
+
+//fun getStringLength(obj: Any): Int? {
+//    if (obj is String) {
+//        // `obj` 在该条件分支内自动转换成 `String`
+//        return obj.length
+//    }
+//
+//    // 在离开类型检测分支后，`obj` 仍然是 `Any` 类型
+//    return null
+//}
+
+
+//fun printProduct(arg1: String, arg2: String) {
+//    val x = parseInt(arg1)
+//    val y = parseInt(arg2)
+//
+//    // 直接使用 `x * y` 会导致编译错误，因为它们可能为 null
+//    if (x != null && y != null) {
+//        // 在空检测后，x 与 y 会自动转换为非空值（non-nullable）
+//        println(x * y)
+//    }
+//    else {
+//        println("'$arg1' or '$arg2' is not a number")
+//    }
+//}
+//
+//fun parseInt(str: String): Int? {
+//    return null
+//}
+
 //带有两个 Int 参数、返回 Int 的函数：
 //fun sum(a: Int, b: Int): Int {
 //    return a + b
