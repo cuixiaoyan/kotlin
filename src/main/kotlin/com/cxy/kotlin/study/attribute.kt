9aad083cce6cd08723c250346a106f9f6e262980
cuixiaoyan <56696508@qq.com>
2020-04-16 16:47:07 +0800

    spring kotlin 类与继承学习完成，属性与字段开始。

--- src/main/kotlin/com/cxy/kotlin/study/attribute.kt
+++ src/main/kotlin/com/cxy/kotlin/study/attribute.kt
@@ -5,14 +5,28 @@ package com.cxy.kotlin.study
  */
 fun main(args: Array<String>) {
 
-    //
-    //
+    //Kotlin 类中的属性既可以用关键字 var 声明为可变的，也可以用关键字 val 声明为只读的。
+    //class Address {
+    //    var name: String = "Holmes, Sherlock"
+    //    var street: String = "Baker"
+    //    var city: String = "London"
+    //    var state: String? = null
+    //    var zip: String = "123456"
+    //}
 
-    //
-    //
+    //要使用一个属性，只要用名称引用它即可：
+    //fun copyAddress(address: Address): Address {
+    //    val result = Address() // Kotlin 中没有“new”关键字
+    //    result.name = address.name // 将调用访问器
+    //    result.street = address.street
+    //    // ……
+    //    return result
+    //}
 
-    //
-    //
+    //Getters 与 Setters 声明一个属性的完整语法是
+    //var <propertyName>[: <PropertyType>] [= <property_initializer>]
+    //    [<getter>]
+    //    [<setter>]
 
     //
     //
